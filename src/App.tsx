4c9273835b96fcd9c8cc16c49046a4c6e1ed4001
juicermv <34755463+juicermv@users.noreply.github.com>
2024-01-17 18:52:01 +0200

    fixed navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,50 @@
 import './App.css'
+import { ReactNode, useState } from 'react'
+
+// Bootstrap
+import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
-import { ReactNode, useState } from 'react'
+
+// Pages
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 
+// Components
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+
 function App() {
-  const [page, setPage] = useState('')
+	const [page, setPage] = useState('')
 
-  function getPage(name: string) : ReactNode{
-    switch (name.toLowerCase()) {
-      case 'home':
-        return <Home/>
-      case 'projects':
-        return <Projects />
-      default: 
-        return <Home/>
-    }
-  }
+	function getPage(name: string): ReactNode {
+		switch (name.toLowerCase()) {
+			case 'home':
+				return <Home />
+			case 'projects':
+				return <Projects />
+			default:
+				return <Home />
+		}
+	}
 
-  return (
-    <>
-      <div className='tw-flex tw-h-full tw-flex-col tw-justify-between tw-content-stretch'>
-        <NavBar title='Juicermv' _default="Home" source={['Home', 'Projects']} onItemClicked={ (item) => {setPage(item.toLowerCase())}} onTitleClicked={()=> {}}/>
-        <div className='tw-w-full tx-h-full'>
-          {
-            getPage(page)
-          }
-        </div>
-        <Footer />
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className='tw-flex tw-h-full tw-flex-col tw-justify-between tw-content-stretch'>
+				<NavBar
+					title='Juicermv'
+					_default='Home'
+					source={['Home', 'Projects']}
+					onItemClicked={(item) => {
+						setPage(item.toLowerCase())
+					}}
+					onTitleClicked={() => {}}
+				/>
+				<div className='tw-w-full tx-h-full'>{getPage(page)}</div>
+				<Footer />
+			</div>
+		</>
+	)
 }
 
 export default App
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,26 @@
 export default function Home() {
-    
-
-    return <>
-        <div className="p-4">
-            <div className="px-4 tw-flex tw-flex-row tw-flex-nowrap text-bg-primary bg-primary tw-items-center shadow-sm rounded">
-                <img src="https://github.com/juicermv.png" className="tw-flex-none" />
-                <div className="tw-flex tw-flex-col tw-content-stretch">
-                    <h1>Welcome.</h1>
-                    <span className="text-wrap">
-                        My name is juicermv.
-                        I use my free time to code various projects (including this website).
-
-                        <br/>
-                        I am proficient with C++, C#, Python, and Node.js.
-                        <br/>
-                        Head to <b>Projects</b> to see my works.
-                    </span>
-                </div>
-            </div>
-        </div>
-    </>
+	return (
+		<>
+			<div className='p-4'>
+				<div className='px-4 tw-flex tw-flex-row tw-flex-nowrap text-bg-primary bg-primary tw-items-center shadow-sm rounded'>
+					<img
+						src='https://github.com/juicermv.png'
+						alt='GitHub Profile Picture'
+						className='tw-flex-none'
+					/>
+					<div className='tw-flex tw-flex-col tw-content-stretch'>
+						<h1>Welcome.</h1>
+						<span className='text-wrap'>
+							My name is juicermv. I use my free time to code
+							various projects (including this website).
+							<br />
+							I am proficient with C++, C#, Python, and Node.js.
+							<br />
+							Head to <b>Projects</b> to see my works.
+						</span>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
