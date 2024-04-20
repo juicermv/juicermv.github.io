c39f68439e800e59c77245ed806641b9a18f815d
juicermv <34755463+juicermv@users.noreply.github.com>
2024-04-20 18:21:04 +0300

    added numake to projects list

--- src/pages/GPS.tsx
+++ src/pages/GPS.tsx
@@ -6,19 +6,20 @@ import { Link } from 'react-router-dom'
 interface GPSProps {
 	setCurrent: (current: string) => void
 }
-export default function GPS({setCurrent}:GPSProps) {
+export default function GPS({ setCurrent }: GPSProps) {
 	setCurrent('GPS')
 	return (
 		<>
 			<div className='p-4 tw-flex tw-flex-row tw-flex-wrap text-bg-body bg-body tw-items-center tw-justify-evenly gap-4'>
 				<div className='tw-flex tw-flex-col tw-content-stretch text-center'>
 					<div className='tw-flex-col tw-p-2 tw-flex tw-items-center tw-justify-center'>
-						<h1>
-						San Andreas: GPS
+						<h1>San Andreas: GPS</h1>
+						<h1 className='tw-text-xs rounded border border-primary tw-p-1 text-primary'>
+							<i className='bi bi-car-front-fill tw-mr-1' />
+							REDUX
 						</h1>
-						<h1 className='tw-text-xs rounded border border-primary tw-p-1 text-primary'><i className='bi bi-car-front-fill tw-mr-1'/>REDUX</h1>
 					</div>
-					
+
 					<span className='text-wrap'>
 						The definitive GPS mod for Grand Theft Auto: San
 						Andreas. <br /> Providing full navigation both for
@@ -35,29 +36,29 @@ export default function GPS({setCurrent}:GPSProps) {
 						></Link>
 					</div>
 				</div>
-				<div className='tw-flex tw-flex-row tw-justify-center gap-4 tw-items-center tw-flex-wrap tw-mt-4'>
-						<div className='card'>
-							<img
-								src={sc1}
-								className='card-img'
-								alt="GTA SA's Radar, showing a player-set red objective marker, with a navigation route leading to it."
-							/>
-						</div>
-						<div className='card'>
-							<img
-								src={sc3}
-								className='card-img'
-								alt="GTA SA's Radar, showing a yellow mission objective marker with a yellow navigation route leading to it."
-							/>
-						</div>
-						<div className='card'>
-							<img
-								src={sc2}
-								className='card-img'
-								alt="GTA SA's Radar, showing a blue marker, signifying a friendly mission NPC, with a blue navigation route leading to it."
-							/>
-						</div>
+				<div className='tw-flex tw-flex-row tw-justify-center gap-4 tw-items-center tw-flex-wrap'>
+					<div className='card'>
+						<img
+							src={sc1}
+							className='card-img'
+							alt="GTA SA's Radar, showing a player-set red objective marker, with a navigation route leading to it."
+						/>
 					</div>
+					<div className='card'>
+						<img
+							src={sc3}
+							className='card-img'
+							alt="GTA SA's Radar, showing a yellow mission objective marker with a yellow navigation route leading to it."
+						/>
+					</div>
+					<div className='card'>
+						<img
+							src={sc2}
+							className='card-img'
+							alt="GTA SA's Radar, showing a blue marker, signifying a friendly mission NPC, with a blue navigation route leading to it."
+						/>
+					</div>
+				</div>
 			</div>
 		</>
 	)
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,18 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 interface ProjectsProps {
 	setCurrent: (current: string) => void
 }
-export default function Projects({setCurrent}:ProjectsProps) {
+export default function Projects({ setCurrent }: ProjectsProps) {
 	setCurrent('Other Projects')
 	return (
 		<>
-			<div className='p-4 tw-flex tw-flex-row tw-flex-wrap text-bg-body bg-body tw-items-center tw-justify-between gap-4'>
-				<div className='tw-flex tw-flex-col tw-content-stretch text-center tw-m-auto'>
-					<h1>
-						<i className='bi bi-gear-wide-connected tw-mr-2'></i>
-						Projects
-					</h1>
+			<div className='p-4 tw-flex tw-flex-row tw-flex-wrap text-bg-body bg-body tw-items-center tw-justify-evenly gap-4'>
+				<div className='tw-flex tw-flex-col tw-content-stretch text-center'>
+					<h1>Projects.</h1>
 					<span className='text-wrap'>
 						Here you can find the random things I spend my free time
 						on. <br />
@@ -21,6 +18,29 @@ export default function Projects({setCurrent}:ProjectsProps) {
 				</div>
 
 				<div className='tw-flex tw-flex-row tw-justify-center gap-4 tw-items-center tw-flex-wrap'>
+					<div className='card'>
+						<div className='card-body'>
+							<h5 className='card-title'>
+								<i className='bi bi-gear-wide-connected tw-mr-2'></i>
+								NuMake
+							</h5>
+
+							<p className='card-text'>
+								A brand new build system for C/C++.
+								<div className='p-2 tw-mt-2 border rounded border-warning-subtle bg-warning-subtle text-bg-warning-subtle mono tw-text-xs'>
+									Extremely early, WIP!
+								</div>
+							</p>
+
+							<div className='btn-group'>
+								<Link
+									to='https://github.com/juicermv/numake'
+									className='btn btn-outline-primary bi bi-github '
+								/>
+							</div>
+						</div>
+					</div>
+
 					<div className='card'>
 						<div className='card-body'>
 							<h5 className='card-title'>
