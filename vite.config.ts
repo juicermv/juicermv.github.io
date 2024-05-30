cd80a59f531d9bc193564eaab0994918d106665a
Yoni <yonimeller@protonmail.com>
2024-05-30 04:23:40 +0300

    small changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,32 @@ export default defineConfig({
   plugins: [react()],
   build: {
     minify: 'terser',
-    cssMinify: 'lightningcss'
+    cssMinify: 'lightningcss',
+    terserOptions: {
+      module: true,
+      toplevel: true,
+      compress: {
+        booleans_as_integers: true,
+        hoist_funs: true,
+        module: true,
+        toplevel: true,
+        passes: 5,
+        unsafe: true,
+        unsafe_Function: true,
+        unsafe_math: true,
+      },
+      mangle: {
+        eval: true,
+        toplevel: true,
+        module: true
+      }
+    }
+  },
+  css :{
+    lightningcss : {
+      minify: true,
+      analyzeDependencies: true,
+      
+    }
   }
 })
